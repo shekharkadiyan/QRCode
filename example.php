54fca584c98ef6810f9a0a7562b8684e466b51bc
laptop <arvind.123.dagar@gmail.com>
2015-05-26 15:16:57 +0530

    change

--- example.php
+++ example.php
@@ -1,15 +1,12 @@
 <?php
 /*************************************************************
- * This script is developed by Arturs Sosins aka ar2rsawseen, http://webcodingeasy.com
+ 
  * Fee free to distribute and modify code, but keep reference to its creator
  *
  * This class generate QR [Quick Response] codes with proper metadata for mobile  phones
  * using google chart api http://chart.apis.google.com
  * Here are sources with free QR code reading software for mobile phones:
- * http://reader.kaywa.com/
- * http://www.quickmark.com.tw/En/basic/download.asp
- * http://code.google.com/p/zxing/
- *
+ 
  * For more information, examples and online documentation visit: 
  * http://webcodingeasy.com/PHP-classes/QR-code-generator-class
  **************************************************************/
